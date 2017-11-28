fa67d130ab873cc7dc6064bda8a2965f6321aec4
CosmicCandi <kalea.wolff@gmail.com>
2017-11-28 15:32:43 -0500

    Adds created_last_month endpoint

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,30 +1,43 @@
 class CompaniesController < ApplicationController
-
   # Get the companies list, except when we're modifying a single record
-  before_action :set_companies_list, except: [:edit, :show, :update, :destroy]
+  before_action :set_companies_list, except: %i[edit show update destroy]
 
   def index
     # Get a list of all companies
-    render json: {data: @companies}
+    render json: { data: @companies }
   end
 
   def alphabetically
     # Render Company names sorted Alphabetically
-    render json: {data: @companies.order(name:"ASC")
-    }
+    render json: { data: @companies.order(name: 'ASC') }
   end
 
   def with_modern_plan
     # Render companies with a modern plan
     # AKA Plan Level ID is not 1 or 2
-    render json: {data: @companies.where(plan_level_id: [3,4,5,6])
-                        .order(name: "ASC")}
+    render json: { data: @companies.where(plan_level_id: [3, 4, 5, 6])
+                                   .order(name: 'ASC') }
   end
 
   def not_trialing
     # Render only companies without active trials
     # AKA trial_status == nil
-    render json: {data: @companies.where(trial_status: nil)}
+    render json: { data: @companies.where(trial_status: nil) }
+  end
+
+  def created_last_month
+    # Use Date.current.beginning_of_month to get the starting day of the month
+    # Then subtract one month from it to get last month's beginning day
+    beginning_of_last_month = (Date.current.beginning_of_month - 1.month)
+
+    # Use Date.current.end_of_month to get the ending day of the month
+    # Then subtract one month from it to get last month's ending day
+    end_of_last_month = (Date.current.end_of_month - 1.month)
+
+    # Finally render the list of companies created between the beginning and
+    # end of last month
+    render json: { data: @companies.where(created_at:
+                           beginning_of_last_month..end_of_last_month) }
   end
 
   private
@@ -33,5 +46,4 @@ class CompaniesController < ApplicationController
     # Get this once so we don't have to keep getting it
     @companies = Company.all
   end
-
 end
--- db/seeds/02_companies.rb
+++ db/seeds/02_companies.rb
@@ -7,7 +7,8 @@ if companies.length < 5
         plan_level_id: PlanLevel.find(1).id },
       { name: 'Things are Getting Dicey',
         trial_status: (Date.today + 15),
-        plan_level_id: PlanLevel.find(3).id },
+        plan_level_id: PlanLevel.find(3).id,
+        created_at: Date.new(2017, 10, 15) },
       { name: 'The Future is Bright',
         trial_status: (Date.today + 20),
         plan_level_id: PlanLevel.find(6).id },
@@ -16,7 +17,8 @@ if companies.length < 5
         plan_level_id: PlanLevel.find(4).id },
       { name: 'AlbaCORE',
         # Trial Status intentionally left blank
-        plan_level_id: PlanLevel.find(2).id }
+        plan_level_id: PlanLevel.find(2).id,
+        created_at: Date.new(2017, 10, 28) }
     ]
   )
 end
